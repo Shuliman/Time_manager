97a18c4b6ab2c95729bdef2b831f5bbd2a73bedf
Shuliman <Julik.schuldiner@proton.me>
2023-02-12 20:42:14 +0100

    Rewrite API's & writen documentation

--- back-end/API/getAllData.php
+++ back-end/API/getAllData.php
@@ -3,6 +3,9 @@ namespace api;
 
 include "time.php";
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL); 
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 $Time = new Time();
