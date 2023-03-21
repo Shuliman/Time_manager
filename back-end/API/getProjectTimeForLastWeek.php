ca72d986e2e2040543cf44289f891d3a6572b676
Shuliman <Julik.schuldiner@proton.me>
2023-03-21 16:00:16 +0100

    Deleted unnecessary stupid implementation of APIs

--- back-end/API/getProjectTimeForLastWeek.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace api;
-
-include "time.php";
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-$Time = new Time();
-echo $Time->getProjectTimeForLastWeek();
